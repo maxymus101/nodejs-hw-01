0536ad8b0fa3329d9795cdf22a9ffec2a0efd516
Max Akimov <maxymus2828@gmail.com>
2025-07-27 18:46:17 +0200

    create, read, write contacts functions

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts, null, 2);
+  try {
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    console.log('Дані успішно записані у файл.');
+  } catch (error) {
+    console.error('Помилка запису у файл:', error);
+  }
+};
